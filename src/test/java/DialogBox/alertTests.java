fd2768fae47ec6a71fc12bc4f7730e240381abc6
Pothin-Neza <pothin.neza2023@kepler.org>
2025-12-09 15:21:38 +0200

    added assertion to alertTest

--- src/test/java/DialogBox/alertTests.java
+++ src/test/java/DialogBox/alertTests.java
@@ -1,7 +1,10 @@
 package DialogBox;
 
 import base.BaseTests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class alertTests extends BaseTests {
     @Test
@@ -10,6 +13,8 @@ public class alertTests extends BaseTests {
     public void alertTest(){
         var alert = homePage.clickDialogBox();
         alert.triggerAlert();
+        String alertMessage = alert.getAlertText();
         alert.alert_clickOk();
+        assertEquals(alertMessage, "Hello world!", "wrong alert message");
     }
 }
