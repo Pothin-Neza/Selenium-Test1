db424ddc6d38bec9e3c3e6c223272cbaf8d11fe2
Pothin-Neza <pothin.neza2023@kepler.org>
2026-02-05 12:28:47 +0200

    updated BaseTests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,25 +3,46 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
+import java.time.Duration;
+
 public class BaseTests {
-    private WebDriver driver;
+
+    protected WebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Detect CI environment (GitHub Actions sets CI=true)
+        boolean isCI = System.getenv("CI") != null;
+
+        if (isCI) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
         homePage = new HomePage(driver);
     }
 
     @AfterClass
-    public void tearDown() throws InterruptedException{
-        Thread.sleep(3000);
-        driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
